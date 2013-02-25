82408b52742ec331cb1c4912d3afd483fb0d415d
Wojciech Kasperek <wojciechksprk@gmail.com>
2013-02-25 14:03:31 +0100

    pierwsze zajęcia

---
+++ wspak.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void wspak(char wyraz[20], int i)
+{
+  if (wyraz[i] != '\0')
+  {
+    wspak(wyraz, i + 1);
+  	cout << wyraz[i];
+  }
+}
+
+int main()
+{
+  char wyraz[20];
+
+  cout << "Podaj wyraz: ";
+  cin >> wyraz;
+  wspak(wyraz, 0);
+  cout << endl;
+
+  return 0;
+}
